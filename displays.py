3a8131ee4382ef7f029c93e3844e62ab2d8a0671
Alejandro Lazarte <chealote@gmail.com>
2025-03-01 16:54:02 -0300

    update the current utils to match refactor

--- displays.py
+++ displays.py
@@ -1,17 +1,13 @@
 import re
-import sys
-import subprocess
+from utils import main
 
 status_filename = f".{__file__.split('/')[-1]}"
-status_filepath = f"/tmp/{status_filename}"
-set_default_current_display = False
-current_display = ""
+displays = [] # need to keep this to turn off all other displays
 
 def list_of_displays():
     with open("/var/log/Xorg.0.log") as f:
         content = f.read()
 
-    displays = []
     for line in re.findall("Output [a-zA-Z0-9-]+ connected", content):
         m = re.match(".*Output ([a-zA-Z0-9-]+).*", line)
         displays.append(m.group(1) )
@@ -21,24 +17,7 @@ def list_of_displays():
 
     return displays
 
-def get_next_display(displays):
-    current_display = None
-    try:
-        with open(status_filepath) as f:
-            current_display = f.read()
-            if not re.match("[a-zA-Z0-9-]+", current_display):
-                raise Exception("Current display doesn't match")
-    except FileNotFoundError:
-        current_display = displays[0]
-
-    if displays[-1] == current_display:
-        return displays[0]
-    for i, d in enumerate(displays):
-        if d == current_display:
-            return displays[i+1]
-    raise Exception("Couldn't find the new display to make primary")
-
-def format_xrandr_cmd(displays, new_current_display):
+def format_xrandr_cmd(new_current_display):
     xrandr_cmd = "xrandr "
     for d in displays:
         if d == new_current_display:
@@ -49,26 +28,4 @@ def format_xrandr_cmd(displays, new_current_display):
     xrandr_cmd_split.remove("")
     return xrandr_cmd_split
 
-def main():
-    displays = list_of_displays()
-
-    if len(displays) == 1:
-        sys.exit(0)
-
-    new_current_display = get_next_display(displays)
-
-    cmd = format_xrandr_cmd(displays, new_current_display)
-    try:
-        r = subprocess.run(cmd)
-        if r.returncode != 0:
-            print("Failed to run command:", cmd)
-            sys.exit(1)
-    except Exception as e:
-        print(f"Failed to run command: {e}")
-        sys.exit(1)
-
-    with open(status_filepath, "w") as f:
-        f.write(new_current_display)
-
-if __name__ == "__main__":
-    main()
+main(status_filename, list_of_displays, format_xrandr_cmd)
--- layouts.py
+++ layouts.py
@@ -1,36 +1,11 @@
-import subprocess
-import sys
+from utils import main
 
 status_filename = f".{__file__.split('/')[-1]}"
-status_filepath = f"/tmp/{status_filename}"
-default_current_layout = "us"
 
-layouts = ["us", "latam"] # eñe!
+def list_layouts():
+    return ["us", "latam"] # eñe!
 
+def format_cmd(next_layout):
+    return f"setxkbmap -option 'ctrl:nocaps' {next_layout}".split(" ")
 
-try:
-    with open(status_filepath) as f:
-        current_layout = f.read()
-except FileNotFoundError:
-    current_layout = default_current_layout
-
-if current_layout == "":
-    current_layout = default_current_layout
-
-next_layout = ""
-
-if current_layout == layouts[-1]:
-    next_layout = layouts[0]
-else:
-    for i, l in enumerate(layouts):
-        if l == current_layout:
-            next_layout = layouts[i+1]
-
-with open(status_filepath, "w") as f:
-    f.write(next_layout)
-
-try:
-    subprocess.Popen(f"setxkbmap -option 'ctrl:nocaps' {next_layout}".split(" "))
-except Exception as e:
-    print(f"Failed to switch layouts to {next_layout}")
-    sys.exit(1)
+main(status_filename, list_layouts, format_cmd)
